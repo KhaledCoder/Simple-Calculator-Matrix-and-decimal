a27deb463c33c21988b8b63d39755e11c96d1c95
khaledalshref <112747694+khaledalshref@users.noreply.github.com>
2023-12-25 01:44:25 +0200

    Update CalculatorDecimal.java

--- CalculatorDecimal.java
+++ CalculatorDecimal.java
@@ -1,22 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.myproject;
+
+/**
+ *
+ * @author New 1000 Meeza
+ */
 public class CalculatorDecimal {
-    public static double add(double number1, double number2) {
-        return number1+number2;
+    
+    public static double add(double num, double add) {
+        return num+add;
     }
-    public static double sub(double number1, double number2) {
-        return number1-number2;
+
+    public static double sub(double num, double sub) {
+        return num-sub;
     }
-    public static double multi(double number1, double number2) {
-        return number1*number2;
+
+    public static double multi(double multi, double num) {
+        return multi*num;
     }
-    public static double div(double number1, double number2)
-    {
-        return number1/number2;
+
+    public static double div(double num, double div) {
+        return num/div;
     }
-    public static double pow(double number , double pow){
+
+    public static double pow(double number, double pow) {
         double res = number;
-        for(int i = 1; i < pow; i++)
-        {
+        for (int i = 1; i < pow; i++) {
             res *= number;
         }
         return res;
