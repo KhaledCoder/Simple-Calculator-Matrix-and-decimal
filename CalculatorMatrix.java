7e016de42b1c207d85b6169b4409feddd85ea71c
khaledalshref <112747694+khaledalshref@users.noreply.github.com>
2023-12-25 01:44:31 +0200

    Update CalculatorMatrix.java

--- CalculatorMatrix.java
+++ CalculatorMatrix.java
@@ -1,45 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.myproject;
 
+/**
+ *
+ * @author New 1000 Meeza
+ */
 public class CalculatorMatrix {
-    public static int[][] add(int [][]Matrix1 , int [][]Matrix2)
-    {
-        int SumMatrix[][] = new int[Matrix1.length][Matrix1[0].length];
-        for(int i = 0; i < Matrix1.length; i++)
-        {
-            for(int j = 0; j < Matrix1[i].length; j++)
-            {
-                SumMatrix[i][j] = Matrix1[i][j]+ Matrix2[i][j];
+
+    public static int[][] add(int[][] SumMatrix, int[][] Matrix) {
+        for (int i = 0; i < Matrix.length; i++) {
+            for (int j = 0; j < Matrix[i].length; j++) {
+                SumMatrix[i][j] += Matrix[i][j];
             }
         }
         return SumMatrix;
     }
-    public static int[][] subtraction(int [][]Matrix1 , int [][]Matrix2)
-    {
-        int SumMatrix[][] = new int[Matrix1.length][Matrix1[0].length];
-        for(int i = 0; i < Matrix1.length; i++)
-        {
-            for(int j = 0; j < Matrix1[i].length; j++)
-            {
-                SumMatrix[i][j] = Matrix1[i][j]- Matrix2[i][j];
+
+    public static int[][] subtraction(int[][] SumMatrix, int[][] Matrix) {
+        for (int i = 0; i < Matrix.length; i++) {
+            for (int j = 0; j < Matrix[i].length; j++) {
+                SumMatrix[i][j] -= Matrix[i][j];
             }
         }
         return SumMatrix;
     }
-    public static int[][] multiply(int [][]Matrix1 , int [][]Matrix2)
-    {
-        int RowMatrix1 =  Matrix1.length, ColumnMatrix1 = Matrix1[0].length;
+    public static int[][] multiply(int[][] Matrix1, int[][] Matrix2) {
+        int RowMatrix1 = Matrix1.length, ColumnMatrix1 = Matrix1[0].length;
         int RowMatrix2 = Matrix2.length, ColumnMatrix2 = Matrix2[0].length;
         int SumMatrix[][] = new int[RowMatrix1][ColumnMatrix2];
-        for(int i = 0; i < RowMatrix1; i++)
-        {
-            for(int j = 0; j < ColumnMatrix2; j++)
-            {
-                for(int k = 0; k < ColumnMatrix1; k++)
-                {
+        for (int i = 0; i < RowMatrix1; i++) {
+            for (int j = 0; j < ColumnMatrix2; j++) {
+                for (int k = 0; k < ColumnMatrix1; k++) {
                     SumMatrix[i][j] += Matrix1[i][k] * Matrix2[k][j];
                 }
             }
         }
         return SumMatrix;
-    }    
+    }
 }
