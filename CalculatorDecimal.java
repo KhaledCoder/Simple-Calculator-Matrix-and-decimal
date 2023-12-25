f8eb09c5d7065a780899752b374c8b1d2d69ef8b
khaledalshref <112747694+khaledalshref@users.noreply.github.com>
2023-12-25 14:06:05 +0200

    Update CalculatorDecimal.java

--- CalculatorDecimal.java
+++ CalculatorDecimal.java
@@ -1,29 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.myproject;
 
-/**
- *
- * @author New 1000 Meeza
- */
 public class CalculatorDecimal {
-    
+
     public static double add(double num, double add) {
-        return num+add;
+        return num + add;
     }
 
     public static double sub(double num, double sub) {
-        return num-sub;
+        return num - sub;
     }
 
     public static double multi(double multi, double num) {
-        return multi*num;
+        return multi * num;
     }
 
     public static double div(double num, double div) {
-        return num/div;
+        return num / div;
     }
 
     public static double pow(double number, double pow) {
@@ -32,5 +24,38 @@ public class CalculatorDecimal {
             res *= number;
         }
         return res;
+    }
+
+    public static double square(double num) {
+        double result = Math.sqrt(num);
+        return result;
+    }
+
+    public static double fact(int num) {
+        int sum = 1;
+        {
+            for (int i = 1; i <= num; i++) {
+                sum *= i;
+            }
+        }
+        return sum;
+    }
+
+    public static double cos(double num) {
+        double b = Math.toRadians(num);
+        double sum = Math.cos(b);
+        return sum;
+    }
+
+    public static double sin(double num) {
+        double b = Math.toRadians(num);
+        double sum = Math.sin(b);
+        return sum;
+    }
+
+    public static double tan(double num) {
+        double b = Math.toRadians(num);
+        double sum = Math.tan(b);
+        return sum;
     }
 }
