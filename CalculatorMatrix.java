f1b22d9f2a3b5cc2482a70db5cf743a22a271c5d
khaledalshref <112747694+khaledalshref@users.noreply.github.com>
2023-12-25 14:07:31 +0200

    Update CalculatorMatrix.java

--- CalculatorMatrix.java
+++ CalculatorMatrix.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.myproject;
 
-/**
- *
- * @author New 1000 Meeza
- */
 public class CalculatorMatrix {
 
     public static int[][] add(int[][] SumMatrix, int[][] Matrix) {
